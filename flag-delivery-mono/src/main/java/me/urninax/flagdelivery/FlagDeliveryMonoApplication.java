eb510fe18265eba51e079db1ec2569de9a56467c
Maksym Shylo <mrfreezediables@gmail.com>
2025-07-20 02:51:45 +0200

    Create project

---
+++ flag-delivery-mono/src/main/java/me/urninax/flagdelivery/FlagDeliveryMonoApplication.java
@@ -0,0 +1,13 @@
+package me.urninax.flagdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagDeliveryMonoApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(FlagDeliveryMonoApplication.class, args);
+    }
+
+}
---
+++ flag-delivery-mono/src/test/java/me/urninax/flagdelivery/FlagDeliveryMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.urninax.flagdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagDeliveryMonoApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
