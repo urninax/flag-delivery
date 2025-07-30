745598888dfc0f8d0ac890b678270d6467e4c8a8
Maksym Shylo <mrfreezediables@gmail.com>
2025-07-30 14:46:13 +0200

    Implement AccessTokenPrincipal for AccessToken authentication

---
+++ flag-delivery-mono/src/main/java/me/urninax/flagdelivery/user/security/principals/AccessTokenPrincipal.java
@@ -0,0 +1,18 @@
+package me.urninax.flagdelivery.user.security.principals;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AccessTokenPrincipal{
+    private UUID ownerId;
+    private UUID organisationId;
+    private Collection<? extends GrantedAuthority> authorities;
+}
