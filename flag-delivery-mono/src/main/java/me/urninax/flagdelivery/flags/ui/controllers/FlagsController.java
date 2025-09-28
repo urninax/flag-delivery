38024522eb7f941e86cefbcf4dd741abb69ec53d
Maksym Shylo <mrfreezediables@gmail.com>
2025-09-28 23:51:08 +0200

    Fix :)

--- flag-delivery-mono/src/main/java/me/urninax/flagdelivery/flags/ui/controllers/FlagsController.java
+++ flag-delivery-mono/src/main/java/me/urninax/flagdelivery/flags/ui/controllers/FlagsController.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import me.urninax.flagdelivery.flags.services.FlagsService;
 import me.urninax.flagdelivery.flags.shared.FeatureFlagDTO;
 import me.urninax.flagdelivery.flags.ui.requests.CreateFeatureFlagRequest;
+import me.urninax.flagdelivery.organisation.models.membership.OrgRole;
+import me.urninax.flagdelivery.shared.security.enums.AuthMethod;
+import me.urninax.flagdelivery.shared.utils.annotations.RequiresAuthMethod;
+import me.urninax.flagdelivery.shared.utils.annotations.RequiresRole;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/projects/{projectKey}/flags")
 @RequiredArgsConstructor
+@RequiresAuthMethod(AuthMethod.ACCESS_TOKEN)
 public class FlagsController{
     private final FlagsService flagsService;
 
     @PostMapping
+    @RequiresRole(OrgRole.WRITER)
     public ResponseEntity<?> createFlag(@PathVariable("projectKey") String projectKey,
                                         @Valid CreateFeatureFlagRequest request){
         FeatureFlagDTO flagDTO = flagsService.createFlag(projectKey, request);
